7e31e49ed49326ea56f6f197eb7993fa7ae5e624
cari-404 <135954797+cari-404@users.noreply.github.com>
2023-06-14 00:34:59 +0700

    Update build-start.sh

--- build-start.sh
+++ build-start.sh
@@ -21,21 +21,3 @@ git clone --depth=1 https://github.com/TianWalkzzMiku/AK3-4.4.git -b lavender-dt
 git clone --depth=1 https://github.com/TianWalkzzMiku/AK3-4.4.git -b whyred-dtb ak3-whyred
 git clone --depth=1 https://github.com/TianWalkzzMiku/AK3-4.4.git -b tulip-dtb ak3-tulip
 git clone --depth=1 https://github.com/TianWalkzzMiku/AK3-4.4.git -b a26x-dtb ak3-a26x
-
-# Telegram setup
-push_message() {
-    curl -s -X POST \
-        https://api.telegram.org/bot"{$BOT_TOKEN}"/sendMessage \
-        -d chat_id="${CHAT_ID}" \
-        -d text="$1" \
-        -d "parse_mode=html" \
-        -d "disable_web_page_preview=true"
-}
-
-# Push message to telegram
-push_message "
-<b>======================================</b>
-<b>Start Building :</b> <code>SuperRyzen Kernel</code>
-<b>Linux Version :</b> <code>$(make kernelversion | cut -d " " -f5 | tr -d '\n')</code>
-<b>Source Branch :</b> <code>$(git rev-parse --abbrev-ref HEAD)</code>
-<b>======================================</b> "
